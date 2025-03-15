87e414a3b63b4b3445c36692f29cb22458174a5a
Henrique Garcia <henriquegarcia.dev@gmail.com>
2025-03-15 10:41:27 -0300

    feat: Save

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const fetch = require('node-fetch')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
@@ -12,6 +12,10 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend funcionando com Realtime Database!' })
 })
 
-app.listen(port, () => {
-  console.log(`Proxy rodando em http://localhost:${port}`)
-})
+if (!process.env.VERCEL) {
+  app.listen(port, () => {
+    console.log(`Proxy rodando em http://localhost:${port}`)
+  })
+}
+
+module.exports = app
